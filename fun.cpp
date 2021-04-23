79a2788480c0ff1de5593a3d6cd5a0c5ddc6120c
Rofunction <yujiannhn@gmail.com>
2021-04-23 09:03:48 +0800

    modified

--- fun.cpp
+++ fun.cpp
@@ -1,6 +1,24 @@
 #include <iostream>
 #include <string>
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+using namespace std;
+class base
+{
+public: virtual int func()=0;
+};
+class son :public base
+{public:son(int a ,int b)
+    {
+        m_a=a; m_b=b;
+    }
+public: int func()
+    {
+        return m_a+m_b;
+    }
+public:int m_a,m_b;
+};
+int main(){
+    son s(10,20);
+    int x=s.func();
+    cout<<x<<endl;
     return 0;
 }
